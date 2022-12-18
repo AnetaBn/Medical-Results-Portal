49c150a95ac8068f62277f9a5fed09b59ec97384
Hanna Smach <01140716@pw.edu.pl>
2022-12-18 23:03:09 +0100

    connect with database

---
+++ dataBaseSQL.sql
@@ -0,0 +1,48 @@
+DROP DATABASE IF EXISTS pro_database;
+
+CREATE DATABASE pro_database;
+
+USE pro_database;
+
+-- instrukcje do czyszczenia, jesli takie tabele istnieja
+DROP TABLE IF EXISTS doctor;
+DROP TABLE IF EXISTS study;
+DROP TABLE IF EXISTS patient;
+
+-- instrukcje do tworzenia odpowiednich tabel
+
+CREATE TABLE patient
+(
+	-- klucz glowny
+	patient_id SERIAL, 
+	patient_name VARCHAR(50),
+	age int,
+	gender VARCHAR(4),
+	PRIMARY KEY(patient_id)
+);
+
+CREATE TABLE doctor
+(
+	-- klucz glowny
+	doctor_id SERIAL, 
+	doctor_name VARCHAR(50),
+	specialization VARCHAR(50),
+	PRIMARY KEY (doctor_id)
+);
+
+CREATE TABLE study
+(
+	-- klucz glowny
+	study_id SERIAL, 
+	hospital VARCHAR(64),
+	study_date DATE,
+	modality VARCHAR(10),
+    pathFile VARCHAR(100),
+	-- klucz obcy
+	patient_id BIGINT UNSIGNED,
+	doctor_id BIGINT UNSIGNED,
+
+    PRIMARY KEY(study_id),
+	FOREIGN KEY (patient_id) REFERENCES patient(patient_id),
+    FOREIGN KEY (doctor_id) REFERENCES doctor(doctor_id)
+);
--- project_telem/settings.py
+++ project_telem/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,13 +76,23 @@ WSGI_APPLICATION = 'project_telem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pro_database', # database name
+        'USER': 'root',
+        'PASSWORD': 'LQSyM8',
+        'HOST': 'localhost',  # lub 'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
