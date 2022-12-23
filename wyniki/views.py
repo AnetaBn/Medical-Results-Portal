ddcfe3ebb650cdf8cc1e712541eb47b08135a6d9
AdaSawilska <01143808@pw.edu.pl>
2022-12-23 20:15:04 +0100

    frontend changes: history of examinations, edit form (not saving changes)

--- wyniki/views.py
+++ wyniki/views.py
@@ -1,16 +1,17 @@
-import re
-from django.utils.timezone import datetime
-from django.http import HttpResponse
 from django.shortcuts import render
-from django import template
+from django.utils.timezone import datetime
 from legacy.models import Doctor, Patient, Study
+
+
 # from .models import Doctor, Patient, Study
 
 def home(request):
     return render(request, "wyniki/home.html")
 
+
 def about(request):
     return render(request, "wyniki/about.html")
+
 
 def hello_there(request, name):
     return render(
@@ -34,27 +35,64 @@ def add_results(request, name):
     )
 
 
-def create_study(request, name):
-    if request.method == 'POST':
-        if request.POST.get('patient_id') and request.POST.get('hospital') and request.POST.get('date') and request.POST.get('type'):
-            one_study=Study()
-            one_study.hospital = request.POST.get('hospital')
-            one_study.study_date = request.POST.get('date')
-            one_study.modality = request.POST.get('type')
-            one_study.patient = Patient.objects.get(patient_id = request.POST.get('patient_id'))
-            one_study.doctor = Doctor.objects.get(doctor_name = 'John Watson') # to change 
-            one_study.save()
-            msge ="Data inserted to study table with id: " + str(one_study.study_id)
-        else:
-            msge=name
-    else:
-        msge=name
+def edit_results(request, name, result_id):
+    edited_result = Study.objects.get(study_id=result_id)
 
-    return render(request,'wyniki/add_results.html',
+    return render(
+        request,
+        'wyniki/edit_results.html',
         {
-            'msg': msge,
+            'name': name,
+            'msg': name,
+            'result': edited_result,
+
         }
     )
 
+
+def create_study(request, name):
+    if request.method == 'POST':
+        if request.POST.get('patient_id') and request.POST.get('hospital') and request.POST.get(
+                'date') and request.POST.get('type'):
+            one_study = Study()
+            one_study.hospital = request.POST.get('hospital')
+            one_study.study_date = request.POST.get('date')
+            one_study.modality = request.POST.get('type')
+            one_study.patient = Patient.objects.get(patient_id=request.POST.get('patient_id'))
+            one_study.doctor = Doctor.objects.get(doctor_name='John Watson')  # to change
+            one_study.save()
+            msge = "Data inserted to study table with id: " + str(one_study.study_id)
+        else:
+            msge = name
+    else:
+        msge = name
+
+    return render(request, 'wyniki/add_results.html',
+                  {
+                      'msg': msge,
+                  }
+                  )
+
+
 # HOW TO DELETE - USEFUL COMMAND!!!
 # https://stackoverflow.com/questions/3805958/how-to-delete-a-record-in-django-models
+
+def history(request, name):
+    results_list = Study.objects.all()
+    doctor_list = []
+    doctor_results = []
+    for r in results_list:
+        doctor_name = r.doctor.doctor_name.replace(" ", "")
+        doctor_list.append(doctor_name)
+        if doctor_name == name:
+            doctor_results.append(r)
+
+    return render(
+        request,
+        'wyniki/history.html',
+        {
+            'results_list': results_list,
+            'doctor_list': doctor_list,
+            'doctor_results': doctor_results,
+        }
+    )
