2210049434c80ef8a3d80b73f3c94e40be22013e
Aneta <73952373+AnetaBn@users.noreply.github.com>
2023-01-27 13:04:50 +0100

    Add files via upload

---
+++ initDB.sql
@@ -0,0 +1,5 @@
+-- SQLite
+INSERT INTO doctor (doctor_name, specialization) VALUES ('Anna May', 'radiology'); 
+INSERT INTO doctor (doctor_name, specialization) VALUES ('John Watson', 'general medicine');
+INSERT INTO patient (patient_name, age, gender) VALUES ('Sherlock Holmes', 40, 'male');
+
